d3cd17d0ebe7cfff9ca78ced09428b4bfa74a7d4
lxj16 <lxjlee16@gmail.com>
2020-01-19 14:12:56 -0800

    basic template

--- discounts/urls.py
+++ discounts/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from discountsApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.main_page),
+    path('discounts/', include('discountsApp.urls')),
 ]
--- discountsApp/views.py
+++ discountsApp/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse, HttpResponseRedirect
+from . import views
+from .forms import signUpForm
+from .models import User
 
 # Create your views here.
+
+
+def main_page(request):
+    return render(request, 'discountsApp/mainPage.html')
+
+
+def index(request):
+    return render(request, 'discountsApp/index.html')
+
+
+def login(request):
+    return render(request, 'discountsApp/login.html')
+
+
+def signup(request):
+    form = signUpForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        new_user = form.save()
+        return HttpResponseRedirect('/')
+    # form = forms.signUpForm()
+    # if request == 'POST':
+    #     form = forms.signUpForm(request.POST)
+    #     if form.is_valid():
+
+    #         user_name = form.cleaned_data.get('user_name')
+    #         email = form.cleaned_data.get('email')
+    #         password = form.cleaned_data.get('password')
+    #         new_user = User(name=user_name, password=password, email=email)
+    #         new_user.save()
+
+    #     else:
+    #         form = forms.signUpForm()
+    return render(request, 'discountsApp/signUp.html', {'form': form})
+
+
+def forgotPassword(request):
+    return render(request, 'discountsApp/forgotPassword.html')
